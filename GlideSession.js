0278fa6972c8d03c7df131752c26a7e4afbca46c
Tim Woodruff <myemailaddressisveryclever@gmail.com>
2018-09-24 12:24:27 -0700

    Modified GlideSession and replaces session.js with it

--- GlideSession.js
+++ GlideSession.js
@@ -3,12 +3,12 @@ GlideSession.prototype = {
 	
 	/**
 	 * Impersonate a user for the duration of the session (INCLUDING AFTER THE SCRIPT IS FINISHED RUNNING!), or until the onlineImpersonate() method is called again, with another (or the original user's) user_id.
+	 * @param userID {string} The user_id OR the sys_id of the user you'd like to impersonate for this session (e.g. 'admin' or '6816f79cc0a8016401c5a33be04be441').
+	 * @return The sys_id of the CURRENT user (PRE-impersonation). This can later be passed into session.onlineImpersonate() in order to reimpersonate the original session, which prevents ending up stuck in the other user's session after the script is finished running.
 	 * @example
 	 * var originalUserID = session.onlineImpersonate('abel_tuter');
 	 * //script here, executes as the impersonated user
 	 * session.onlineImpersonate(originalUser);
-	 * @param userID {string} The user_id OR the sys_id of the user you'd like to impersonate for this session (e.g. 'admin' or '6816f79cc0a8016401c5a33be04be441').
-	 * @return The sys_id of the CURRENT user (PRE-impersonation). This can later be passed into session.onlineImpersonate() in order to reimpersonate the original session, which prevents ending up stuck in the other user's session after the script is finished running.
 	 */
 	onlineImpersonate: function(userID) {
 		return 'abc123';
